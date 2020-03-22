f7d38a813b7641c94332dafcd64c603d09baf365
yshay <yshay@cuidas.com.br>
2020-03-22 17:52:57 -0300

    Updates

---
+++ precache-manifest.e9787851a72279b37881c682fdd5d403.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9821465a48bcefb9e56229a2211fb3cb",
+    "url": "/park-pass/index.html"
+  },
+  {
+    "revision": "2ced69a37275fd13e3db",
+    "url": "/park-pass/static/css/main.deff4103.chunk.css"
+  },
+  {
+    "revision": "5e241b6eb92961c15fa1",
+    "url": "/park-pass/static/js/2.c92dc190.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/park-pass/static/js/2.c92dc190.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ced69a37275fd13e3db",
+    "url": "/park-pass/static/js/main.96d9e896.chunk.js"
+  },
+  {
+    "revision": "beca07d6267072b425d3",
+    "url": "/park-pass/static/js/runtime-main.87dc1c7f.js"
+  },
+  {
+    "revision": "a1deb1f63c28d4c9189ca888916cf2ce",
+    "url": "/park-pass/static/media/car_green.a1deb1f6.svg"
+  },
+  {
+    "revision": "7b10909159b263545004fcc34d8e24b2",
+    "url": "/park-pass/static/media/car_red.7b109091.svg"
+  },
+  {
+    "revision": "a64433de184f6a513c9550b11d2a2fbf",
+    "url": "/park-pass/static/media/ivana-cajina-WPVtT0MEM00-unsplash.a64433de.jpg"
+  },
+  {
+    "revision": "5f5d286889153058a7dbe1a40a923a8f",
+    "url": "/park-pass/static/media/john-matychuk-yvfp5YHWGsc-unsplash.5f5d2868.jpg"
+  }
+]);
